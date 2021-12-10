ee905d17708a414edc6d30181bbd6135427f6b7b
ChaoChi-719 <j0988993487@gmail.com>
2021-12-10 19:02:02 +0800

    modify foc.cpp mode 0

--- FOC/foc.cpp
+++ FOC/foc.cpp
@@ -242,6 +242,10 @@ void torque_control(ControllerStruct *controller)
 
     if (controller->mode == 0)
     {
+        if (controller->p_goal < controller->theta_mech)
+        {
+            controller->p_goal += 6.28;
+        }
         if ((abs(controller->p_goal - controller->theta_mech) < temp) || controller->flag == 1)
         {
             error = controller->p_goal - controller->theta_mech;
